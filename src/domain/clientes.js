7b52a108dbec949bd7b710a52e8e7242817f0167
unknown <jkalicha@gmail.com>
2024-05-30 11:02:17 -0300

    cliente validacion

--- src/domain/clientes.js
+++ src/domain/clientes.js
@@ -1,34 +1,44 @@
 export class clientes {
 
-    #nombre;
-    #mail;
-  
-    constructor(aNombre) {
-      this.#nombre = aNombre;
-    }
-  
-    getNombre() {
-      return this.#nombre;
-    }
-  
-    setCapital(aMail) {
-      this.#mail = aMail;
-    }
-  
-    toString() {
-      return `País: ${this.#nombre} - capital: ${this.#mail}`;
-    }
-  
-    isValid() {
-      if (this.#nombre === undefined || this.#nombre === null || this.#nombre === '') {
-        throw new Error('El nombre del país no puede ser vacío');
-      }
-      if (this.#mail === undefined || this.#mail === null || this.#mail === '') {
-        throw new Error('El mail del cliente no puede ser vacío');
-      }
-      return true;
-    }
-    //validar que este bien escrito el nombre, nada de numeros.
-    
-    // validar que este escrito bien el mail.
+  #nombre;
+  #mail;
+
+  constructor(aNombre) {
+    this.#nombre = aNombre;
   }
+
+  getNombre() {
+    return this.#nombre;
+  }
+
+  setCapital(aMail) {
+    this.#mail = aMail;
+  }
+
+  toString() {
+    return `Cliente: ${this.#nombre} - Mail: ${this.#mail}`;
+  }
+
+  isValid() {
+    if (this.#nombre === undefined || this.#nombre === null || this.#nombre === '') {
+      throw new Error('El nombre del cliente no puede ser vacío');
+    }
+    if (this.#mail === undefined || this.#mail === null || this.#mail === '') {
+      throw new Error('El mail del cliente no puede ser vacío');
+    }
+
+    // Validar que el nombre no contenga números
+    const nombreSinNumeros = /^[a-zA-Z\s]+$/;
+    if (!nombreSinNumeros.test(this.#nombre)) {
+      throw new Error('El nombre del cliente no puede contener números');
+    }
+
+    // Validar que el mail esté bien formateado
+    const mailFormato = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!mailFormato.test(this.#mail)) {
+      throw new Error('El mail del cliente no está bien formateado');
+    }
+
+    return true;
+  }
+}
