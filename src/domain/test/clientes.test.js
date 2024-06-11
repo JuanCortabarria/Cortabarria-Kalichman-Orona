a36b36e7b7aa84d4b83460b28ecf5388ed6e63b3
unknown <jkalicha@gmail.com>
2024-06-11 18:05:36 -0300

    test canchas

--- src/domain/test/clientes.test.js
+++ src/domain/test/clientes.test.js
@@ -1,20 +1,22 @@
 // clientes.test.js
-import { clientes } from './clientes';
+import { clientes } from '../clientes';
 
-describe('clientes', () => {
-  test('should create a valid clientes object', () => {
-    const cliente = new clientes('Juan Perez');
+describe('clientes class tests', () => {
+  test('Create a valid clientes object', () => {
+    let cliente = new clientes('Juan Perez');
     cliente.setCapital('juan@example.com');
     expect(cliente.getNombre()).toBe('Juan Perez');
     expect(cliente.toString()).toBe('Cliente: Juan Perez - Mail: juan@example.com');
     expect(cliente.isValid()).toBe(true);
   });
 
-  test('should throw error for invalid clientes object', () => {
+  test('Throw error for invalid clientes object', () => {
+    let expectedErrorMessage = 'El nombre del cliente no puede ser vacío';
+    let cliente = new clientes('');
+    cliente.setCapital('invalidemail');
     expect(() => {
-      const cliente = new clientes('');
-      cliente.setCapital('invalidemail');
       cliente.isValid();
-    }).toThrow();
+    }).toThrow(expectedErrorMessage);
   });
 });
+
