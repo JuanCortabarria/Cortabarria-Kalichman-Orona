00705be7580dce5e7ab942f8b6a5d61c18310173
unknown <jkalicha@gmail.com>
2024-06-11 18:08:30 -0300

    unitarios lista res

--- src/domain/test/listaReservas.test.js
+++ src/domain/test/listaReservas.test.js
@@ -1,11 +1,11 @@
-//listaReservas.test.js
-import { listaReservas } from './listaReservas';
+// listaReservas.test.js
+import { listaReservas } from '../listaReservas';
 import { reservas } from './reservas';
 
-describe('listaReservas', () => {
-  test('should add a valid reserva to the list', () => {
-    const lista = new listaReservas();
-    const reserva = new reservas('Cancha 1');
+describe('listaReservas class tests', () => {
+  test('Add a valid reserva to the list', () => {
+    let lista = new listaReservas();
+    let reserva = new reservas('Cancha 1');
     reserva.setFecha('2023-06-12');
     reserva.setHora('10:00');
     reserva.setNombreCliente('Juan Perez');
@@ -14,16 +14,17 @@ describe('listaReservas', () => {
     expect(lista.getReservas().length).toBe(1);
   });
 
-  test('should throw error for duplicate reserva', () => {
-    const lista = new listaReservas();
-    const reserva = new reservas('Cancha 1');
+  test('Throw error for duplicate reserva', () => {
+    let lista = new listaReservas();
+    let reserva = new reservas('Cancha 1');
     reserva.setFecha('2023-06-12');
     reserva.setHora('10:00');
     reserva.setNombreCliente('Juan Perez');
     reserva.setMailCliente('juan@example.com');
     lista.add(reserva);
+    let expectedErrorMessage = "No se pudo agregar. Cancha 1 ya está en la lista.";
     expect(() => {
       lista.add(reserva);
-    }).toThrow();
+    }).toThrow(expectedErrorMessage);
   });
 });
