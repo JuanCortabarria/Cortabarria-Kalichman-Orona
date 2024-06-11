432306c899ed90cf29adfbe8ad44283e03be6531
unknown <jkalicha@gmail.com>
2024-06-11 18:04:44 -0300

    test canchas

--- src/domain/test/canchas.test.js
+++ src/domain/test/canchas.test.js
@@ -1,19 +1,20 @@
 // canchas.test.js
-import { Canchas } from './canchas';
+import { Canchas } from '../canchas';
 
-describe('Canchas', () => {
-  test('should create a valid Canchas object', () => {
-    const cancha = new Canchas('Futbol', 100, 'Ubicacion 1', 'Descripcion 1');
-    expect(cancha.getNombre()).toBe('Futbol');
+describe('Canchas class tests', () => {
+  test('Create a valid Canchas object', () => {
+    let cancha = new Canchas('Padel', 100, 'Ubicacion 1', 'Descripcion 1');
+    expect(cancha.getNombre()).toBe('Padel');
     expect(cancha.getPrecio()).toBe(100);
     expect(cancha.getUbicacion()).toBe('Ubicacion 1');
     expect(cancha.getDescripcion()).toBe('Descripcion 1');
     expect(cancha.isValid()).toBe(true);
   });
 
-  test('should throw error for invalid Canchas object', () => {
+  test('Throw error for invalid Canchas object', () => {
+    let expectedErrorMessage = "El nombre de la cancha no puede ser vacío";
     expect(() => {
-      new Canchas('', -1, '', '');
-    }).toThrow();
+      new Canchas('', -1, '', '').isValid();
+    }).toThrow(expectedErrorMessage);
   });
 });
