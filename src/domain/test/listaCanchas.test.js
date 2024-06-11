045c14ed749704b11679c59da73f810210a71f97
unknown <jkalicha@gmail.com>
2024-06-11 18:06:23 -0300

    test listaCanchas

--- src/domain/test/listaCanchas.test.js
+++ src/domain/test/listaCanchas.test.js
@@ -1,21 +1,23 @@
 // listaCanchas.test.js
-import { ListaCanchas } from './listaCanchas';
-import { Canchas } from './canchas';
+import { ListaCanchas } from '../listaCanchas';
+import { Canchas } from '../canchas';
 
-describe('ListaCanchas', () => {
-  test('should add a valid cancha to the list', () => {
-    const lista = new ListaCanchas();
-    const cancha = new Canchas('Futbol', 100, 'Ubicacion 1', 'Descripcion 1');
+describe('ListaCanchas class tests', () => {
+  test('Add a valid cancha to the list', () => {
+    let lista = new ListaCanchas();
+    let cancha = new Canchas('Padel', 100, 'Ubicacion 1', 'Descripcion 1');
     lista.add(cancha);
     expect(lista.getCanchas().length).toBe(1);
   });
 
-  test('should throw error for duplicate cancha', () => {
-    const lista = new ListaCanchas();
-    const cancha = new Canchas('Futbol', 100, 'Ubicacion 1', 'Descripcion 1');
+  test('Throw error for duplicate cancha', () => {
+    let lista = new ListaCanchas();
+    let cancha = new Canchas('Padel', 100, 'Ubicacion 1', 'Descripcion 1');
     lista.add(cancha);
+    let expectedErrorMessage = "No se pudo agregar. Padel ya está en la lista.";
     expect(() => {
       lista.add(cancha);
-    }).toThrow();
+    }).toThrow(expectedErrorMessage);
   });
 });
+
